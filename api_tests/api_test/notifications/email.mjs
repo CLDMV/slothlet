0a30290cd18c6d4d4fb94ce932ed7d151741b81a
Nate Corcoran <Shinrai@users.noreply.github.com>
2026-05-27 19:32:12 -0700

    test(fixtures): add multi-default notifications fixture to api_test for debug comparison

---
+++ api_tests/api_test/notifications/email.mjs
@@ -0,0 +1,29 @@
+/**
+ *	@Project: @cldmv/slothlet
+ *	@Filename: /api_tests/api_test_multi_default/notifications/email.mjs
+ *	@Date: 2026-05-27 19:05:26 -07:00 (1779933926)
+ *	@Author: Nate Corcoran <CLDMV>
+ *	@Email: <Shinrai@users.noreply.github.com>
+ *	-----
+ *	@Last modified by: Nate Corcoran <CLDMV> (Shinrai@users.noreply.github.com)
+ *	@Last modified time: 2026-05-27 19:10:12 -07:00 (1779934212)
+ *	-----
+ *	@Copyright: Copyright (c) 2013-2026 Catalyzed Motivation Inc. All rights reserved.
+ */
+
+/**
+ * Fixture for Rule 5 / C02: file WITH a default export in a multi-default folder.
+ * Gets preserved as its own namespace: api.notifications.email(...)
+ */
+
+/**
+ * Send an email notification.
+ * @param {string} to - Recipient address.
+ * @param {string} msg - Message body.
+ * @returns {{ sent: boolean, via: string, to: string }} Result object.
+ * @example
+ * email("a@x", "hi"); // { sent: true, via: "email", to: "a@x" }
+ */
+export default function send(to, msg) {
+	return { sent: true, via: "email", to, msg };
+}
--- src/lib/handlers/module-manager.mjs
+++ src/lib/handlers/module-manager.mjs
@@ -1,7 +1,13 @@
 /**
  *	@Project: @cldmv/slothlet
  *	@Filename: /src/lib/handlers/module-manager.mjs
+ *	@Date: 2026-05-27T11:22:33-07:00 (1779906153)
  *	@Author: Nate Corcoran <CLDMV>
+ *	@Email: <Shinrai@users.noreply.github.com>
+ *	-----
+ *	@Last modified by: Nate Corcoran <CLDMV> (Shinrai@users.noreply.github.com)
+ *	@Last modified time: 2026-05-27 18:57:20 -07:00 (1779933440)
+ *	-----
  *	@Copyright: Copyright (c) 2013-2026 Catalyzed Motivation Inc. All rights reserved.
  */
 
